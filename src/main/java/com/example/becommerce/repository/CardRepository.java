d030b6a6e191621cd5ea92f492c5b524e9e24eb2
skaimarvas <skaimarvas@gmail.com>
2024-03-02 14:46:26 +0300

    CardRepository was created

---
+++ src/main/java/com/example/becommerce/repository/CardRepository.java
@@ -0,0 +1,7 @@
+package com.example.becommerce.repository;
+
+import com.example.becommerce.entity.Card;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CardRepository extends JpaRepository<Card,Long> {
+}
