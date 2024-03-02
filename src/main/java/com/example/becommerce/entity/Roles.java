561c96fbfc1eb1e8806531c91fdaa00bdb219ffb
skaimarvas <skaimarvas@gmail.com>
2024-03-02 10:29:49 +0300

    roles's fields were created

--- src/main/java/com/example/becommerce/entity/Roles.java
+++ src/main/java/com/example/becommerce/entity/Roles.java
@@ -1,4 +1,18 @@
 package com.example.becommerce.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "roles", schema = "becommerce")
 public class Roles {
+
+    private long id;
+    private String name;
+    private String code;
+
 }
